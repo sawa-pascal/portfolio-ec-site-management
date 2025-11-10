286631a31bf701103a7b6abc20d7b0701ada61d9
sawa <sawa@pascal.jp>
2025-11-10 15:20:20 +0900

    誰が何を買ったかの情報を見れるようにページの仮実装

---
+++ src/app/sales/sales-detail/sales-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-detail',
+  imports: [],
+  templateUrl: './sales-detail.component.html',
+  styleUrl: './sales-detail.component.scss',
+})
+export class SalesDetailComponent {
+
+}
---
+++ src/app/sales/sales-list/sales-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-list',
+  imports: [],
+  templateUrl: './sales-list.component.html',
+  styleUrl: './sales-list.component.scss',
+})
+export class SalesListComponent {
+
+}
