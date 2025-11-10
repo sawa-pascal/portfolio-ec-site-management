e591da86fa8cdc08d80b8f0662959e9f30a633c5
sawa <sawa@pascal.jp>
2025-11-10 16:13:43 +0900

    sales-listの実装

--- src/app/sales/sales-detail/sales-detail.component.ts
+++ src/app/sales/sales-detail/sales-detail.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Sales } from '../sales-model';
+import { SalesService } from '../../services/sales.service';
 
 @Component({
   selector: 'app-sales-detail',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sales-detail.component.html',
   styleUrl: './sales-detail.component.scss',
 })
-export class SalesDetailComponent {
+export class SalesDetailComponent implements OnInit {
+  viewSales: Sales[] = [];
+  searchText: FormControl = new FormControl('');
+  searchMailText: FormControl = new FormControl('');
+  pageIndex: number = 0;
+  pageSize: number = 10;
 
+  private sales: Sales[] = [];
+
+  constructor(private salesService: SalesService, private router: Router) {}
+
+  ngOnInit() {
+    this.salesService.requestGetSalesList().subscribe({
+      next: (res: any) => {
+        if (res.success) {
+          this.sales = this.viewSales = res.sales as Sales[];
+        }
+      },
+      error: () => {
+        console.log('requestGet失敗');
+      },
+    });
+  }
+
+  searchSales() {
+    // this.viewSales = this.sales.filter((sale) => {
+    //   if (!this.searchText.value && !this.searchMailText.value) return true;
+
+    //   const name_query = String(this.searchText.value).toLowerCase();
+    //   const email_query = String(this.searchMailText.value).toLowerCase();
+
+    //   return (
+    //     sale.name.toLowerCase().includes(name_query) &&
+    //     sale.email.toLowerCase().includes(email_query)
+    //   );
+    // });
+  }
 }
--- src/app/sales/sales-list/sales-list.component.ts
+++ src/app/sales/sales-list/sales-list.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { PagerComponent } from '../../pager/pager.component';
+import { Sales } from '../sales-model';
+import { SalesService } from '../../services/sales.service';
+import { UsersService } from '../../services/users.service';
+import { Users } from '../../users/users-model';
 
 @Component({
   selector: 'app-sales-list',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink, PagerComponent],
   templateUrl: './sales-list.component.html',
   styleUrl: './sales-list.component.scss',
 })
-export class SalesListComponent {
+export class SalesListComponent implements OnInit {
+  viewSales: Sales[] = [];
+  searchText: FormControl = new FormControl('');
+  pageIndex: number = 0;
+  pageSize: number = 10;
 
+  private sales: Sales[] = [];
+
+  constructor(private salesService: SalesService, private usersService: UsersService) {}
+
+  ngOnInit() {
+    this.salesService.requestGetSalesList().subscribe({
+      next: (res: any) => {
+        if (res.success) {
+          this.sales = this.viewSales = res.sales as Sales[];
+        }
+      },
+      error: () => {
+        console.log('requestGet失敗');
+      },
+    });
+
+    this.usersService.updateUsers();
+  }
+
+  searchSales() {
+    this.viewSales = this.sales.filter((sale) => {
+      if (!this.searchText.value) return true;
+
+      let userName = this.usersService.getUsers().find((_) => _.id == sale.user_id)?.name;
+      if (!userName) return true;
+
+      const name_query = String(this.searchText.value).toLowerCase();
+      return userName.toLowerCase().includes(name_query);
+    });
+  }
+
+  getUsers(): Users[] {
+    return this.usersService.getUsers();
+  }
+
+  convertUserName(id: number): string {
+    return this.usersService.convertUserName(id);
+  }
 }
